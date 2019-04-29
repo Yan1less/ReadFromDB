8312184781ddfe7924d5348b56121f4cdd6c6cfd
Yan1less <895072956@qq.com>
2019-04-29 10:16:37 +0800

    注册到注册中心

--- src/main/java/com/yan1less/readdb/ReaddbApplication.java
+++ src/main/java/com/yan1less/readdb/ReaddbApplication.java
@@ -5,6 +5,7 @@ import com.yan1less.readdb.aop.DynamicDataSourceAnnotationInterceptor;
 import com.yan1less.readdb.register.DynamicDataSourceRegister;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Import(DynamicDataSourceRegister.class)
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableDiscoveryClient
 public class ReaddbApplication {
     @Bean
     public DynamicDataSourceAnnotationAdvisor dynamicDatasourceAnnotationAdvisor() {
